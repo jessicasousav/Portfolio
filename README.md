993612537185c6bc1afcb6860ca029d324a0d2ca
Jessica de Sousa Vieira <jessiesousavi@gmail.com>
2025-03-10 11:54:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Portfólio
+
+[![NPM](https://img.shields.io/npm/l/react)](https://github.com/jessicasousav/Portfolio/blob/main/LICENSE)
+
+# Sobre
+
+- Acesse: #
+
+Neste portfólio, você poderá encontrar as informações sobre a Desenvolvedora Full Stack [Jéssica de Sousa Vieira](https://linkedin.com/in/jessica-sousa-vieira), a qual está em seu 2º semestre da faculdade de ADS.
+
+# Tecnologias utilizadas
+
+Para este portfólio foram utilizados conhecimentos em:
+
+## Front-end
+
+- HTML5
+- CSS3
+- JavaScript
+
+## Autor
+
+Jéssica de Sousa Vieira
+
+https://linkedin.com/in/jessica-sousa-vieira
---
+++ src/script.js
@@ -0,0 +1,37 @@
+function showMenu() {
+    if(itensMenu.style.display == 'flex') {
+        itensMenu.style.display = 'none';
+    } else {
+        itensMenu.style.display = 'flex';
+    }
+};
+
+function changeBigmenu(){
+    if(window.innerWidth >= 767){
+        itensMenu.style.display = 'flex';
+    } else {
+        itensMenu.style.display = 'none';
+    }
+}
+
+function hideMenu(){
+    if(window.innerWidth <= 767){
+        itensMenu.style.display = 'none';
+    }
+}
+
+
+// MENU ATIVO
+
+let menuItem = document.querySelectorAll('.item-menu')
+
+function activeLink() {
+    menuItem.forEach((item) => {
+        item.classList.remove('ativo');
+    })
+    this.classList.add('ativo');
+}
+
+menuItem.forEach((item) => {
+    item.addEventListener('click', activeLink);
+});
